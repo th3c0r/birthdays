13044e246121b4c420585e39de2827144260250c
th3c0r <th3c0r@gmail.com>
2023-03-18 21:11:06 +0200

    Make menu testable

--- src/menu.rs
+++ src/menu.rs
@@ -1,4 +1,5 @@
 use std::io;
+#[derive(PartialEq, Debug)]
 pub enum MenuOptions {
     ADD,
     SEARCH,
@@ -7,13 +8,26 @@ pub enum MenuOptions {
     QUIT,
 }
 
+fn parse_menu_input(input: &str) -> Result<MenuOptions, &'static str> {
+    match input.trim().parse() {
+        Ok(num) => match num {
+            1 => Ok(MenuOptions::ADD),
+            2 => Ok(MenuOptions::SEARCH),
+            3 => Ok(MenuOptions::LOAD),
+            4 => Ok(MenuOptions::SAVE),
+            5 => Ok(MenuOptions::QUIT),
+            _ => Err("Invalid option, please enter a number between 1 and 5."),
+        },
+        Err(_) => Err("Invalid input, please enter a valid number"),
+    }
+}
+
 pub fn menu() -> MenuOptions {
     let stdin = io::stdin();
 
     //loop until valid choice
-    let choice: u8 = loop {
+    loop {
         // clear screen
-
         print!("{}[2J", 27 as char);
 
         println!(" 1. Add a person");
@@ -28,31 +42,30 @@ pub fn menu() -> MenuOptions {
 
         match stdin.read_line(&mut input) {
             Ok(_) => (),
-            Err(_) => {
-                continue;
-            }
+            Err(_) => continue,
         };
 
-        match input.trim().parse() {
-            Ok(num) => {
-                if num < 1 || num > 5 {
-                    continue;
-                }
-                break num;
-            }
-            Err(_) => {
-                println!("Please enter a valid option");
+        match parse_menu_input(&input) {
+            Ok(menu_option) => return menu_option,
+            Err(err_msg) => {
+                println!("{}", err_msg);
                 continue;
             }
-        };
-    };
+        }
+    }
+}
 
-    match choice {
-        1 => MenuOptions::ADD,
-        2 => MenuOptions::SEARCH,
-        3 => MenuOptions::LOAD,
-        4 => MenuOptions::SAVE,
-        5 => MenuOptions::QUIT,
-        _ => todo!(),
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parsing_menu_option() {
+        assert_eq!(parse_menu_input("1\n").unwrap(), MenuOptions::ADD);
+        assert_eq!(parse_menu_input("2\n").unwrap(), MenuOptions::SEARCH);
+        assert!(parse_menu_input("7\n").is_err());
+        assert_eq!(parse_menu_input("4\n").unwrap(), MenuOptions::SAVE);
+        assert_eq!(parse_menu_input("5\n").unwrap(), MenuOptions::QUIT);
+        assert!(parse_menu_input("\n").is_err());
     }
 }
